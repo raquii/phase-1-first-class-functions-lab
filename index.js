090e2dfadb8f05f26cd3edd4ed6d3cdcddca2209
raquel <raquelanaroman@gmail.com>
2021-05-17 18:57:24 -0400

    commit tree b33e1e5f7c6e4b57dd1e2701a4ddfa9e69fbc905

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+//use .slice() non-destructive where first arg is start and second is end
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+}
+
+//.slice() with a negative will take last elements with the argument being the number of elements to retrieve
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier){
+    return function(fare){
+        return fare * multiplier;
+    }
+}
+
+const fareDoubler = function(fare) {
+    return createFareMultiplier(2)(fare);
+}
+
+const fareTripler = function(fare) {
+    return createFareMultiplier(3)(fare);
+}
+
+function selectDifferentDrivers(drivers, fn){
+    return fn(drivers);
+}
